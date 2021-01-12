98a4e04bc74b4dcf66f7e07ebc6f7a9c3a8b7c71
Cristian MACARI <cristian.macari@stud.acs.upb.ro>
2021-01-12 17:14:47 +0200

    Auth

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 
-export default class About extends Component{
-    render(){
-        return(
+export default class About extends Component {
+
+    render() {
+        return (
             <div>
                 <p>About</p>
             </div>
